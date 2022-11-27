b6d61edc227cefbbe1a53bd74b99ec34eb788b2b
C-dub <25037768+ColeS82@users.noreply.github.com>
2022-11-26 23:25:23 -0700

    added the inquirer, console.table and mysql2 npm packages

--- server.js
+++ server.js
@@ -16,20 +16,7 @@ const db = mysql.createConnection(
   console.log(`Connected to the Employee_Tracker_db database.`)
 );
 
-// Query database using COUNT() and GROUP BY
-db.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', function (err, results) {
-  console.log(results);
-});
-
-// Query database using SUM(), MAX(), MIN() AVG() and GROUP BY
-db.query('SELECT SUM(quantity) AS total_in_section, MAX(quantity) AS max_quantity, MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity FROM favorite_books GROUP BY section', function (err, results) {
-  console.log(results);
-});
-
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+db.connect(function (err) {
+  if (err) return console.log(err);
+  InquirerPrompt();
+})
